c4880b2ba98e5dff6731330f600bb3f89f9b41b7
Patrik Švikruha <patrik.svikruha@gmail.com>
2020-11-20 16:39:56 +0100

    Add watcher and router sample

--- src/router/index.js
+++ src/router/index.js
@@ -5,33 +5,50 @@ import GraphView from '../views/GraphView.vue'
 import TableView from '../views/TableView.vue'
 import About from '../views/About.vue'
 
+const user = {
+    name: "Fero",
+    isAuthenticated: true
+}
+
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/GraphView',
-    name: 'GraphView',
-    component: GraphView
-  },
-  {
-    path: '/TableView',
-    name: 'TableView',
-    component: TableView
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: About
-  }
-]
-
 const router = new VueRouter({
-  routes
+    mode: "history",
+    routes: [
+        {
+            path: '/',
+            name: 'Home',
+            component: Home
+        },
+        {
+            path: '/GraphView/:graphId?',
+            name: 'GraphView',
+            component: GraphView
+        },
+        {
+            path: '/TableView',
+            name: 'TableView',
+            component: TableView
+        },
+        {
+            path: '/about',
+            name: 'About',
+            component: About,
+            meta: { requireAuth: true }
+        },
+
+    ],
 })
+
+router.beforeEach(
+    (to, from, next) => {
+        if (to.meta.requireAuth && !user.isAuthenticated) {
+            next({ name: "Login" })
+        }
+        else {
+            next()
+        }
+    }
+)
 
 export default router
